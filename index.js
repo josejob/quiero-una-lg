52255ba55db26dc90655074463abfd58c4984ea9
josejob <jose.m.job@gmail.com>
2021-10-31 01:45:42 +0200

    mas cosas que faltan

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+//TODO: Meter array URLs para procesar mas de una URL por vendor
 // TODO: Volver a configurar Eslint y pretifier
 // TODO: Agregar Amazon, Mediamarkt, Fnac, CorteIngles
+//Amazon: 
+//https://www.amazon.es/LG-OLED55CX3LA-TELEVISOR-4K/dp/B08H5G6732
+//https://www.amazon.es/LG-OLED55CX-ALEXA-Inteligencia-Artificial-Inteligente/dp/B086J31MRW
+//https://www.amazon.es/LG-TV-OLED-55CX6-UHD/dp/B086DCSC3P
+//Fnac:
+//https://www.fnac.es/TV-OLED-55-LG-OLED55CX-4K-UHD-HDR-Smart-TV-TV-TV-OLED/a7365549
+//https://www.fnac.es/TV-OLED-55-LG-OLED55CX3-4K-UHD-HDR-Smart-TV-TV-TV-OLED/a7872041
+//https://www.fnac.es/mp8979831/TV-OLED-55-LG-55CX6-4K-UHD-Smart-TV-negro/w-4
+//CorteIngles:
+//https://www.elcorteingles.es/electronica/A39624761-tv-oled-1388-cm-55-lg-oled55cx6la-4k-inteligencia-artificial-hdr-dolby-vision-reacondicionado-a-estrenar/
+//https://www.elcorteingles.es/electronica/A35445416-tv-oled-1388-cm-55-lg-oled55cx6la-4k-con-inteligencia-artificial-hdr-dolby-vision-iq-y-smart-tv/
+//Worten:
+//https://www.worten.es/productos/tv-video-y-sonido/televisores/oled/tv-lg-oled55cx5-oled-55-140-cm-4k-ultra-hd-smart-tv-7151225
+//Mediamarkt: parece que no tienen ficha de la TV
+//Ebay,es
+//https://www.ebay.es/itm/234174767693?hash=item3685e7624d:g:bVwAAOSwYmhhNfoo
 const { chromium } = require('playwright')
 const maximumPrice = 599
 const shops = [
