e1f2f3d986f0966c2514a43713fbf8bd50e71f7b
jose_fhios <jose.martinez@fhios.es>
2021-11-16 19:00:09 +0100

    perf(console): change don't open navigator

--- fichar.js
+++ fichar.js
@@ -2,7 +2,7 @@
 const { chromium } = require("playwright");
 
 (async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
 
   const context = await browser.newContext({
     geolocation: { longitude: 41.411338, latitude: 2.197971 },
